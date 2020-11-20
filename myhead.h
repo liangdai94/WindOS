8b569874495cf19794a540fbc97209da0a3739b1
liangdai94 <1029721846@qq.com>
2020-11-20 23:52:33 +0800

    Changes to be committed: 	new file:   myhead.h

---
+++ myhead.h
@@ -0,0 +1,13 @@
+#ifndef MYHEAD_H_
+#define MYHEAD_H_
+
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<sys/type.h>
+#include<sys/stat.h>
+#include<string.h>
+
+#define MAXLINE 4096
+#endif
